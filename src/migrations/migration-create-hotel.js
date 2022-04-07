b6937afb8dbc48500b924f2443b0b3d9a3bf7060
Tri Le <letri6102@gmail.com>
2022-04-07 13:51:01 +0700

    Tri Le

---
+++ src/migrations/migration-create-hotel.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('hotels', {
+    //   id: DataTypes.STRING,
+    // email: DataTypes.STRING,
+    // firstName: DataTypes.STRING,
+    // lastName: DataTypes.STRING,
+    // address: DataTypes.STRING,
+    // gender: DataTypes.STRING,
+    // roleid: DataTypes.STRING
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      description : { 
+        type: Sequelize.TEXT
+      },
+      image:{
+        type: Sequelize.STRING
+      },
+      name:{
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('hotels');
+  }
+};
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from './route/web';
+import connectDB from './config/connectDB';
 require('dotenv').config();
 
 let app = express();
@@ -13,11 +14,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 viewEngine(app);
 initWebRoutes(app);
+connectDB(app);
 
-let port = process.env.PORT || 6969;
+const server = require('http').createServer();
+let port = process.env.PORT || 8000;
 //Port === undefined => port = 6969
 
 app.listen(port, () => {
     //callback
-    console.log("Backend Nodejs is runing on the port : " + port)
+    console.log("Hotel Management is runing on the port : " + port)
 })
