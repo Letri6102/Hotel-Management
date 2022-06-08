7ed349e35f4e50c8dce568d0d0f3eeb02e161a75
Tri Le <letri6102@gmail.com>
2022-06-08 19:08:21 +0700

    TriLe

--- nodejs/src/services/hotelService.js
+++ nodejs/src/services/hotelService.js
@@ -39,7 +39,7 @@ let getAllHotel = () => {
       let data = await db.Hotel.findAll({});
       if (data && data.length > 0) {
         data.map((item) => {
-          item.image = new Buffer(item.image, "base64").toString("binary");
+          item.image =  Buffer.from(item.image, "base64").toString("binary");
           return item;
         });
       }
--- nodejs/src/services/specialtyService.js
+++ nodejs/src/services/specialtyService.js
@@ -37,7 +37,7 @@ let getAllSpecialty = () => {
       let data = await db.Specialty.findAll({});
       if (data && data.length > 0) {
         data.map((item) => {
-          item.image = new Buffer(item.image, "base64").toString("binary");
+          item.image = Buffer.from(item.image, "base64").toString("binary");
           return item;
         });
       }
